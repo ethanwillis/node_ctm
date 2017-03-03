03803975523c1d4e4a9552be12801d54ba0ec3b0
Ethan Willis <elwillis1@gmail.com>
2017-03-03 16:34:32 -0600

    Added a new gitigore for debug logs.
    Added a script to generate documentation
    
    Created docs for all code currently implemented

---
+++ create_docs.sh
@@ -0,0 +1,5 @@
+mkdir docs
+jsdoc -r numbers_purchase -d docs/numbers_purchase
+jsdoc -r smart_router -d docs/smart_router
+jsdoc -r voice_menu -d docs/voice_menu
+jsdoc -r voice_menu_items -d docs/voice_menu_items
